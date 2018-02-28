ef95e5410c7863813c6209229799f5de3f538ab0
Oana Ursu <oursu@bluechevycorvette.local>
2018-02-27 20:39:59 -0800

    getting ready for pip

---
+++ 3DChromatin_ReplicateQC/__main__.py
@@ -0,0 +1,20 @@
+
+from __future__ import print_function
+import argparse
+import os
+import sys
+from software.genomedisco.genomedisco import concordance_utils
+
+def main():
+    command_methods = {'preprocess': concordance_utils.preprocess,
+                         'concordance': concordance_utils.concordance,
+                         'summary': concordance_utils.summary,
+                         'cleanup':concordance_utils.clean_up,
+                       'run_all': concordance_utils.run_all}
+    
+    command, args = concordance_utils.parse_args_replicateqc()
+    command_methods[command](**args)
+
+
+if __name__ == "__main__":
+    main()
