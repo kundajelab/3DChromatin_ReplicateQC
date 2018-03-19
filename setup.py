b23ed1f9291bcacdcd9264c610bb2c8265be6e59
Oana Ursu <oursu@bluechevycorvette.local>
2018-03-19 00:57:14 -0700

    nodes file to bins file

--- setup.py
+++ setup.py
@@ -4,16 +4,6 @@ from setuptools.command.install import install
 import subprocess as subp
 import os
 
-#from https://stackoverflow.com/questions/42384294/how-to-run-multiple-bash-sh-scripts-in-setup-py-file
-#class MyInstall(install):
-#    def run(self):
-#        repo_dir=os.path.dirname(os.path.realpath(__file__))
-#        software_dir=repo_dir+'/software'
-#        #make init files across the path to genomedisco
-#        open(software_dir+'/__init__.py', 'a').close()
-#        open(software_dir+'/genomedisco/__init__.py', 'a').close()
-#        install.run(self)
-
 
 config = {
 'include_package_data': True,
@@ -22,14 +12,10 @@ config = {
 'version': '0.0.1',
 'packages': ['3DChromatin_ReplicateQC'],
 'setup_requires': [],
-'install_requires': ['numpy>=1.9', 'matplotlib>=1.5.0','h5py','hifive==1.5.6','genomedisco>=0.0.0'],
-#'dependency_links': ["https://github.com/kundajelab/deeplift/tarball/v0.5.1-theano#egg=deeplift-0.5.1-theano",
-# "https://github.com/kundajelab/simdna/tarball/0.3#egg=simdna-0.3"],
+'install_requires': ['numpy>=1.9', 'matplotlib>=1.5.0','h5py','hifive==1.5.6','genomedisco>=1.0.0'],
 'scripts': [],
 'entry_points': {'console_scripts': ['3DChromatin_ReplicateQC = 3DChromatin_ReplicateQC.__main__:main']},
 'name': '3DChromatin_ReplicateQC',
-#'cmdclass': {'install': MyInstall},
-#'package_data': {'': extra_files},
 }
 if __name__== '__main__':
     setup(**config)
