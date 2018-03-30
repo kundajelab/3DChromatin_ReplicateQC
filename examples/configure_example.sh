d63d3bb3ef6a60027f342d75d0337e64c723d907
Oana Maria Ursu <oursu@scg4-ln01.stanford.edu>
2018-03-29 17:33:48 -0700

    updated configure_example

--- examples/configure_example.sh
+++ examples/configure_example.sh
@@ -4,8 +4,8 @@ d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #make metadata for the examples
 metadata_samples=${d}/metadata.samples
-printf "HIC001\t${d}/HIC001.res40000.gz\n" > ${metadata_samples}
-printf "HIC002\t${d}/HIC002.res40000.gz\n" >> ${metadata_samples}
+printf "HIC001\t${d}/HIC001.res50000.gz\n" > ${metadata_samples}
+printf "HIC002\t${d}/HIC002.res50000.gz\n" >> ${metadata_samples}
 
 metadata_pairs=${d}/metadata.pairs
 printf "HIC001\tHIC002\n" > ${metadata_pairs}
